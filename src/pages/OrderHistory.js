095104e6249a8306d7e71cd2d5114c06a6b4b72d
Carl Abel <caweb.cph@gmail.com>
2022-08-29 15:40:21 +0200

    master commit

--- src/pages/OrderHistory.js
+++ src/pages/OrderHistory.js
@@ -1,3 +1,10 @@
 export default function OrderHistory() {
-  return <h1>OrderHistory</h1>;
+  return (
+    <>
+      <h1>OrderHistory</h1>
+      <div>
+        <p>asdasd</p>
+      </div>
+    </>
+  );
 }
