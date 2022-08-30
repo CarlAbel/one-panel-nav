620d1813141061d30036cccf688766a8757f338c
Carl Abel <caweb.cph@gmail.com>
2022-08-30 12:29:26 +0200

    size

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,7 +1,7 @@
 export default function Products() {
   return (
     <>
-      <div className="h-8 w-8 bg-slate-100">
+      <div className="h-8 w-20 bg-slate-100">
         <h1 className="text-white">Products</h1>
       </div>
     </>
