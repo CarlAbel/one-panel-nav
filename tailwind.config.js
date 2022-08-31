96f2953d4b2ad027797e5372d8199d3a04614bbb
CarlAbel <caweb.cph@gmail.com>
2022-08-31 15:53:15 +0200

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          100: "#9576F6",
+          200: "#757EF7",
+          300: "#BBB5F8",
+          400: "#D9D9D990",
+          500: "#A174F7",
+          600: "#000000",
+        },
+      },
+    },
   },
   plugins: [],
-};
+}
