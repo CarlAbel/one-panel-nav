d5b8749b44e3440ff04e1098e99e5b71c66cbf52
CarlAbel <caweb.cph@gmail.com>
2022-08-31 10:29:04 +0200

    products add

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,10 +1,10 @@
-import { useEffect, useState, useContext } from "react";
-import TokenContext from "../context/TokenContext";
+import { useEffect, useState, useContext } from "react"
+import TokenContext from "../context/TokenContext"
 
 export default function Products() {
-  const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([])
 
-  const { token } = useContext(TokenContext);
+  const { token } = useContext(TokenContext)
 
   useEffect(function () {
     fetch("http://localhost:3001/products", {
@@ -14,17 +14,31 @@ export default function Products() {
     })
       .then((res) => res.json())
 
-      .then((data) => setProducts(data));
-  }, []);
+      .then((data) => setProducts(data))
+  }, [])
   return (
     <div>
       <h1>Products</h1>
 
       <article>
         {products.map((product) => (
-          <p className="text-white">{product.eanNumber}</p>
+          <div className="w-60 bg-primary-400">
+            <div>
+              <h1 className="text-white">
+                Product Name: {product.brand} {product.productName}
+              </h1>
+              <p>{product.productCategory}</p>
+              <p>{product.productDesc}</p>
+              <p>{product.stock}</p>
+              <div className="">
+                <p>{product.price}</p>
+                <p>{product.retailPrice}</p>
+              </div>
+            </div>
+          </div>
         ))}
-        <p>DATE ADDED</p>
+
+        {/* <p>DATE ADDED</p>
 
         {products.map((product) => (
           <li className="text-white">{product.productName} Product Name: </li>
@@ -37,8 +51,8 @@ export default function Products() {
 
         {products.map((product) => (
           <p className="text-white">{product.brand}</p>
-        ))}
+        ))} */}
       </article>
     </div>
-  );
+  )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          100: "#9576F6",
+          200: "#000000",
+          300: "#000000",
+          400: "#D9D9D9",
+          500: "#000000",
+          600: "#000000",
+        },
+      },
+    },
   },
   plugins: [],
-};
+}
