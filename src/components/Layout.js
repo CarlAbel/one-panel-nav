0ab0cf54a2985ce7a5ff9c6cfc9f8421c5ac81f4
CarlAbel <caweb.cph@gmail.com>
2022-08-31 17:41:48 +0200

    clean up

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,6 @@
-import { Link, Outlet } from "react-router-dom"
+import { Outlet } from "react-router-dom"
 import "../styles.css"
-import { AnimatePresence, motion, useCycle } from "framer-motion"
+
 import NavMenu from "./NavMenu"
 // import TokenContext from "../context/TokenContext"
 // import { useContext } from "react"
