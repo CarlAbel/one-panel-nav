c63af789e7ed265f9b8dc99f7e8143624994ff89
CarlAbel <caweb.cph@gmail.com>
2022-08-31 10:32:23 +0200

    menu

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,30 @@
-import { Outlet } from "react-router-dom";
-import BurgerNav from "./BurgerNav";
+import { Link, Outlet } from "react-router-dom"
+import BurgerNav from "./BurgerNav"
 
 export default function Layout() {
   return (
     <div>
+      <menu>
+        <li>
+          <Link className="text-white" to="/">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link className="text-white" to="/products">
+            Products
+          </Link>
+        </li>
+        <li>
+          <Link className="text-white" to="/signin">
+            Signin
+          </Link>
+        </li>
+      </menu>
+      <BurgerNav />
       <main>
-        <BurgerNav />
         <Outlet />
       </main>
     </div>
-  );
+  )
 }
