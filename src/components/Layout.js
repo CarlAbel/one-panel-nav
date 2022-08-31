24329a532ae297dde8f0b2defdf60c1f6ebab150
CarlAbel <caweb.cph@gmail.com>
2022-08-31 18:59:45 +0200

    layout changes

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,99 +1,16 @@
-import { Link, Outlet } from "react-router-dom"
+import { Outlet } from "react-router-dom"
 import "../styles.css"
-import { AnimatePresence, motion, useCycle } from "framer-motion"
+
+import NavMenu from "./NavMenu"
 // import TokenContext from "../context/TokenContext"
 // import { useContext } from "react"
 
-const itemVariants = {
-  closed: {
-    opacity: 0,
-  },
-  open: { opacity: 1 },
-}
-
-const sideVariants = {
-  closed: {
-    transition: {
-      staggerChildren: 0.2,
-      staggerDirection: -1,
-    },
-  },
-  open: {
-    transition: {
-      staggerChildren: 0.2,
-      staggerDirection: 1,
-    },
-  },
-}
 export default function Layout() {
   // const { token } = useContext(TokenContext)
 
-  const [open, cycleOpen] = useCycle(false, true)
-
   return (
-    <div className="bg-primary-400">
-      <AnimatePresence>
-        {open && (
-          <motion.menu
-            className="flex flex-row"
-            initial={{ width: 0, height: 0 }}
-            animate={{
-              width: 260,
-              height: "calc(90vh - 6rem)",
-            }}
-            exit={{
-              width: 0,
-              height: "calc(90vh - 6rem)",
-              transition: { delay: 0.7, duration: 0 },
-            }}
-            // className="fixed top-0 left-0"
-          >
-            <motion.div
-              className="container"
-              initial="closed"
-              animate="open"
-              exit="closed"
-              variants={sideVariants}
-            >
-              <motion.li whileHover={{ scale: 1.1 }} variants={itemVariants}>
-                <Link className="text-white" to="/">
-                  Home
-                </Link>
-              </motion.li>
-              <motion.li whileHover={{ scale: 1.1 }} variants={itemVariants}>
-                <Link className="text-white" to="/orderHistory">
-                  Order History
-                </Link>
-              </motion.li>
-              <motion.li whileHover={{ scale: 1.1 }} variants={itemVariants}>
-                <Link className="text-white" to="/messages">
-                  Messages
-                </Link>
-              </motion.li>
-              <motion.li whileHover={{ scale: 1.1 }} variants={itemVariants}>
-                <Link className="text-white" to="/products">
-                  Products
-                </Link>
-              </motion.li>
-              <motion.li whileHover={{ scale: 1.1 }} variants={itemVariants}>
-                <Link className="text-white" to="/customers">
-                  Customers
-                </Link>
-              </motion.li>
-              <motion.li whileHover={{ scale: 1.1 }} variants={itemVariants}>
-                <Link className="text-white" to="/signin">
-                  Sign in
-                </Link>
-              </motion.li>
-            </motion.div>
-          </motion.menu>
-        )}
-      </AnimatePresence>
-      <div className="btn-container">
-        <button className="rounded-full" onClick={cycleOpen}>
-          {open ? "Close" : "Open"}
-        </button>
-      </div>
+    <div>
+      <NavMenu />
       <main>
         <Outlet />
       </main>
