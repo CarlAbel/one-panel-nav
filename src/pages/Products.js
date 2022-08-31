9c1be528a0a46d2c72f103bd57d9dce4741dc025
CarlAbel <caweb.cph@gmail.com>
2022-08-31 17:03:12 +0200

    small change

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,9 +1,3 @@
 export default function Products() {
-  return (
-    <>
-      <div className="h-8 w-20 bg-slate-100">
-        <h1 className="text-white">Products</h1>
-      </div>
-    </>
-  );
+  return <></>
 }
