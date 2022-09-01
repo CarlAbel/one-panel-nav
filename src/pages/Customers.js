474241ef4ffecd8cb01835ec055606ba4a7fd2ee
CarlAbel <caweb.cph@gmail.com>
2022-09-01 11:32:49 +0200

    customers styling

--- src/pages/Customers.js
+++ src/pages/Customers.js
@@ -1,64 +1,65 @@
 import { useEffect, useState, useContext } from "react"
 import TokenContext from "../context/TokenContext"
-import FeatherIcon from "feather-icons-react"
-export default function Products() {
-  const [products, setProducts] = useState([])
+
+export default function Customers() {
+  const [users, setUsers] = useState([])
 
   const { token } = useContext(TokenContext)
 
   useEffect(function () {
-    fetch("http://localhost:3001/products", {
+    fetch("http://localhost:3001/users", {
       headers: {
         authorization: "Bearer " + token,
       },
     })
       .then((res) => res.json())
 
-      .then((data) => setProducts(data))
+      .then((data) => setUsers(data))
   }, [])
+
   return (
     <div className="bg-primary-100">
       <div className="flex flex-col">
         <h1 className="text-white self-center pt-24 text-4xl">Customers</h1>
       </div>
-
-      <article className="p-6">
-        {products.map((product) => (
-          <div className="rounded-3xl w-74 h-72 p-4 bg-primary-400  mb-12">
+      <article className="p-8">
+        {users.map((user) => (
+          <div
+            key={user.userId}
+            className="rounded-3xl w-74 h-72 p-4 bg-primary-400  mb-12"
+          >
             <div className="flex flex-col">
-              <h1 className="text-white">Ean-number: #{product.eanNumber}</h1>
-              <h2 className="text-white">
-                Product Name: {product.brand} {product.productName}
-              </h2>
-              <p className="text-white">{product.productCategory}</p>
-              <p className="lineBreak text-white">{product.productDesc}</p>
-
-              <p className="text-white self-center  solidBorderB ">
-                Stock count: {product.stock}
+              <div className="flex justify-between solidBorderB pb-2 ">
+                <div>
+                  <h1 className="text-white pb-2">
+                    Full name: {user.userName}
+                  </h1>
+                  <p className="text-white text-xs">
+                    Customer joined: {user.joined}
+                  </p>
+                </div>
+                <img className="bg-primary-100 rounded-full w-12 h-12 p-6">
+                  {user.image}
+                </img>
+              </div>
+              <div className="flex items-center gap-4">
+                <div>
+                  <h2 className="text-white pt-1">Email: {user.userEmail}</h2>
+                  <p className="text-white text-xs capitalize">
+                    Phone-Number: {user.phone}
+                  </p>
+                </div>
+              </div>
+              <p className="text-white self text-sm self-center solidBorderB pr-10 pl-4">
+                Stock count: {user.userName}
               </p>
-
               <div className="flex justify-between">
-                <p className="text-white pt-8">{product.price}</p>
-                <p className="text-white pt-8">{product.retailPrice}</p>
+                <p className="text-white pt-6 pl-4">$ {user.userName}</p>
+                <p className="text-white pt-6 pr-4">$ {user.userName}</p>
               </div>
             </div>
           </div>
         ))}
-
-        {/* <p>DATE ADDED</p>
-
-        {products.map((product) => (
-          <li className="text-white">{product.productName} Product Name: </li>
-        ))}
-
-        <i>Line</i>
-        {products.map((product) => (
-          <h1 className="text-white">{product.productName} Product Name: </h1>
-        ))}
-
-        {products.map((product) => (
-          <p className="text-white">{product.brand}</p>
-        ))} */}
       </article>
     </div>
   )
